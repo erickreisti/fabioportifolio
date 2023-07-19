7787e47d305a1ffa36c877013b1e827e4d6e6882
erickreisti <erickreisti@gmail.com>
2023-07-19 19:35:41 -0300

    refactor

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-"strict mode";
+"use strict";
+
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+const navMenu = document.querySelector(".nav__links");
+
+hamburger.addEventListener("click", () => {
+  if (hamburger) {
+    hamburger.classList.add("active");
+    navMenu.classList.add("active");
+    close.classList.add("active");
+  }
+});
+
+close.addEventListener("click", () => {
+  if (close) {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    close.classList.remove("active");
+  }
+});
