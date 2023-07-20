ced0e9dae7c5eb7d8f6cedac0a532a65cb24706a
erickreisti <erickreisti@gmail.com>
2023-07-19 22:58:19 -0300

    refactor

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
 const navMenu = document.querySelector(".nav__links");
 
+// event handler menu hamburguer
 hamburger.addEventListener("click", () => {
   if (hamburger) {
     hamburger.classList.add("active");
